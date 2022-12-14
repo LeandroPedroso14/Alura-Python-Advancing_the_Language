e4807ce0170046252f8a977a8f7ace03bc239b79
Leandro Pedroso <106255930+LeandroPedroso14@users.noreply.github.com>
2022-12-14 14:28:31 -0300

    setting error threshold

--- Python_Orientação a Objetos/forca.py
+++ Python_Orientação a Objetos/forca.py
@@ -8,6 +8,7 @@ def jogar():
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
@@ -16,12 +17,16 @@ def jogar():
         chute = input("Qual letra? ")
         chute = chute.strip()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute.upper() == letra.upper()):
+                    letras_acertadas[index] = letra
+                index = index + 1
+        else:
+            erros = erros + 1
 
+        enforcou = erros == 6
         print(letras_acertadas)
 
     print("Fim do jogo")
